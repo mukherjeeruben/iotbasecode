965109f51ba41b0b4cc3730ecff6631416d40978
mukherjeeruben <76583185+mukherjeeruben@users.noreply.github.com>
2020-12-25 14:21:00 +0530

    Version 1.1
    Code Base Check in - Branch Created

---
+++ Arduino/hand/hand.ino
@@ -0,0 +1,36 @@
+int pinIN=14;
+int pinOUT=2;
+int count=0;
+int value=0;
+void setup()
+{
+  pinMode(pinIN, INPUT);
+  pinMode(pinOUT, OUTPUT);
+}
+void loop()
+{  
+ 
+  value=digitalRead(pinIN);
+  if(value==HIGH)
+  {
+     count++;
+   
+     if(count==2)
+      {
+       digitalWrite(pinOUT, LOW);
+       count=0;
+       delay(1200);
+      }
+     else
+      {
+       digitalWrite(pinOUT, HIGH);
+       delay(1200);
+      }
+      
+  }
+}
+  
+ 
+  
+  
+
